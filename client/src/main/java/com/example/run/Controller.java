e96f8b0345c2ccdf6e3ea77d416e967f93ee4091
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-05-18 00:05:48 +0300

    Added choicebox of languages

--- client/src/main/java/com/example/run/Controller.java
+++ client/src/main/java/com/example/run/Controller.java
@@ -1,6 +1,7 @@
 package com.example.run;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 
@@ -19,9 +20,14 @@ public class Controller {
     @FXML
     private PasswordField passField;
 
+    @FXML
+    private ChoiceBox<String> languages;
+
+    private final String[] strings = {"Русский", "Английский", "Чешский", "Литовский"};
 
     @FXML
     public void initialize() {
         hostField.setPromptText(ResourceBundle.getBundle("Registration").getString("host"));
+        languages.getItems().addAll(strings);
     }
 }
