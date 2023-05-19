62d082b3ed872aa037c38a8b7e9cfa76daf27d8a
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-05-19 17:00:10 +0300

    Added returning value

--- client/src/main/java/com/example/proxy/Validation.java
+++ client/src/main/java/com/example/proxy/Validation.java
@@ -7,34 +7,43 @@ import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class Validation extends ProxyController {
-    public void registerEmpty(Locale locale) {
+
+    public boolean registerEmpty(Locale locale) {
+        boolean result = true;
         String bundle = "properties.Registration";
         TextField host = getField("host");
         if (host.getText().length() == 0) {
             Label label = getField("hostLabel");
             label.setText(ResourceBundle.getBundle(bundle, locale).getString("hostEmpty"));
+            result = false;
         }
 
         TextField port = getField("port");
         if (port.getText().length() == 0) {
             Label label = getField("portLabel");
             label.setText(ResourceBundle.getBundle(bundle, locale).getString("portEmpty"));
+            result = false;
         }
 
         TextField login = getField("login");
         if (login.getText().length() == 0) {
             Label label = getField("loginLabel");
             label.setText(ResourceBundle.getBundle(bundle, locale).getString("loginEmpty"));
+            result = false;
         }
 
         TextField password = getField("password");
         if (password.getText().length() == 0) {
             Label label = getField("passwordLabel");
             label.setText(ResourceBundle.getBundle(bundle, locale).getString("passwordEmpty"));
+            result = false;
         }
+
+        return result;
     }
 
-    public void registerLong(Locale locale) {
+    public boolean registerLong(Locale locale) {
+        boolean result = true;
         String bundle = "properties.Registration";
 
         TextField port = getField("port");
@@ -45,18 +54,23 @@ public class Validation extends ProxyController {
         } catch (NumberFormatException e) {
             Label label = getField("portLabel");
             label.setText(ResourceBundle.getBundle(bundle, locale).getString("portWrongFormat"));
+            result = false;
         }
 
         TextField login = getField("login");
         if (login.getText().length() > 32) {
             Label label = getField("loginLabel");
             label.setText(ResourceBundle.getBundle(bundle, locale).getString("loginLong"));
+            result = false;
         }
 
         TextField password = getField("password");
         if (password.getText().length() > 32) {
             Label label = getField("passwordLabel");
             label.setText(ResourceBundle.getBundle(bundle, locale).getString("passwordLong"));
+            result = false;
         }
+
+        return result;
     }
 }
