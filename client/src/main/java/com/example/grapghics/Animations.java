5a558355d7abf90e8b907c63f0f9cc39d267147d
IvanTarasov <iwan.tarasow2013.gmail.com>
2023-05-20 19:03:38 +0300

    Class for animations

---
+++ client/src/main/java/com/example/grapghics/Animations.java
@@ -0,0 +1,18 @@
+package com.example.grapghics;
+
+import javafx.animation.ScaleTransition;
+import javafx.scene.Node;
+import javafx.util.Duration;
+
+public class Animations {
+
+    public void scaleTransitionIn(Duration duration, Node node, double fromX, double fromY, double toX, double toY) {
+        ScaleTransition transition = new ScaleTransition(duration, node);
+        transition.setFromX(fromX);
+        transition.setFromY(fromY);
+
+        transition.setToX(toX);
+        transition.setToY(toY);
+        transition.play();
+    }
+}
